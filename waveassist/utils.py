edfc04250b307abed8a492024baab2aa42d87e5c
kakshilshah <kakshil.shah@gmail.com>
2025-06-13 00:27:55 +0530

    Changes

--- waveassist/utils.py
+++ waveassist/utils.py
@@ -1,9 +1,25 @@
 import requests
 
 BASE_URL ="https://api.waveassist.io"
-def call_post_api(path, body, files=None) -> tuple:
+def call_post_api(path, body) -> tuple:
     url = f"{BASE_URL}/{path}"
+    headers = { "Content-Type": "application/json" }  # JSON content
 
+    try:
+        response = requests.post(url, json=body, headers=headers)  # Sends proper JSON
+        response_dict = response.json()
+        if str(response_dict.get("success")) == "1":
+            return True, response_dict
+        else:
+            error_message = response_dict.get("message", "Unknown error")
+            return False, error_message
+
+    except Exception as e:
+        print(f"❌ API call failed: {e}")
+        return False, str(e)
+
+def call_post_api_with_files(path, body, files=None) -> tuple:
+    url = f"{BASE_URL}/{path}"
     try:
         response = requests.post(url, data=body, files=files or {})
 
@@ -25,6 +41,7 @@ def call_post_api(path, body, files=None) -> tuple:
     except Exception as e:
         print(f"❌ API call failed: {e}")
         return False, str(e)
+
 
 
 def call_get_api(path, params) -> tuple:
